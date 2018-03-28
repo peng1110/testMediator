d7d72154bfbee9b0800ede85df161ab92caab990
lipeng <136866540@qq.com>
2018-03-28 11:20:26 +0800

    上传空项目

---
+++ studyMediator/studyMediator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  studyMediator
+//
+//  Created by  Peng on 2018/3/28.
+//  Copyright © 2018年  Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ studyMediator/studyMediator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  studyMediator
+//
+//  Created by  Peng on 2018/3/28.
+//  Copyright © 2018年  Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
